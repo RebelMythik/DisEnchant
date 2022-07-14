bf0b1098113927f8d4e5f8192ca7b997142f20e8
RebelMythik <fordjmf@hotmail.com>
2022-07-13 23:04:53 -0400

    Improvement

--- src/main/java/me/rebelmythik/disenchant/guis/DisEnchantGUI.java
+++ src/main/java/me/rebelmythik/disenchant/guis/DisEnchantGUI.java
@@ -5,7 +5,6 @@ import de.themoep.inventorygui.GuiElementGroup;
 import de.themoep.inventorygui.InventoryGui;
 import de.themoep.inventorygui.StaticGuiElement;
 import me.rebelmythik.disenchant.DisEnchant;
-import org.apache.commons.lang.ArrayUtils;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
@@ -19,7 +18,6 @@ public class DisEnchantGUI {
         Player player = pl;
         Enchantment[] enchantmentArray = enchs.keySet().toArray(new Enchantment[enchs.size()]);
 
-
         String[] guiSetup = {
                 "FFFF1FFFF",
                 "ggggggggg",
@@ -30,18 +28,23 @@ public class DisEnchantGUI {
 
         InventoryGui gui = new InventoryGui(plugin, null, "DisEnchant Menu", guiSetup);
         gui.setFiller(new ItemStack(Material.GRAY_STAINED_GLASS, 1));
-        gui.addElement('1', new ItemStack(Material.BOOK), click -> {return true;});
-        gui.addElement('F', new ItemStack(Material.GRAY_STAINED_GLASS_PANE), click -> {return true;});
+        gui.addElement('1', new ItemStack(Material.BOOK), click -> true);
+        gui.addElement('F', new ItemStack(Material.GRAY_STAINED_GLASS_PANE), click -> true);
+
 
         GuiElementGroup enchantList = new GuiElementGroup('g');
         for (int i = 0; i < enchantmentArray.length; i++) {
             final int j = i;
             ItemStack it = new ItemStack(player.getInventory().getItemInMainHand().getType());
+            plugin.getLogger().info(enchantmentArray[j].getName());
             it.addUnsafeEnchantment(enchantmentArray[i], enchs.get(enchantmentArray[i]));
             enchantList.addElement(new DynamicGuiElement('g', (viewer) -> new StaticGuiElement('g', it, click -> {
                 pl.getInventory().getItemInMainHand().removeEnchantment(enchantmentArray[j]);
-                ArrayUtils.remove(enchantmentArray, j);
-                gui.draw();
+                enchs.remove(enchantmentArray, j);
+                for (Enchantment enchantment : enchantmentArray) {
+                    plugin.getLogger().info(enchantment.getName());
+                }
+                gui.draw(pl);
                 return true;
             }, enchantmentArray[j].getName())));
         }
