1532f1a4bfeca4083057997d23321e00b720022b
RebelMythik <fordjmf@hotmail.com>
2022-07-18 15:00:21 -0400

    Update

--- src/main/java/me/rebelmythik/disenchant/guis/DisEnchantGUI.java
+++ src/main/java/me/rebelmythik/disenchant/guis/DisEnchantGUI.java
@@ -9,7 +9,10 @@ import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class DisEnchantGUI {
@@ -27,7 +30,9 @@ public class DisEnchantGUI {
         };
 
         InventoryGui gui = new InventoryGui(plugin, null, "DisEnchant Menu", guiSetup);
-        gui.setFiller(new ItemStack(Material.GRAY_STAINED_GLASS, 1));
+
+        ItemStack stack = new ItemStack(Material.RED_CONCRETE);
+        gui.setFiller(stack);
         gui.addElement('1', new ItemStack(Material.BOOK), click -> true);
         gui.addElement('F', new ItemStack(Material.GRAY_STAINED_GLASS_PANE), click -> true);
 
@@ -39,14 +44,31 @@ public class DisEnchantGUI {
             plugin.getLogger().info(enchantmentArray[j].getName());
             it.addUnsafeEnchantment(enchantmentArray[i], enchs.get(enchantmentArray[i]));
             enchantList.addElement(new DynamicGuiElement('g', (viewer) -> new StaticGuiElement('g', it, click -> {
-                pl.getInventory().getItemInMainHand().removeEnchantment(enchantmentArray[j]);
-                enchs.remove(enchantmentArray, j);
-                for (Enchantment enchantment : enchantmentArray) {
-                    plugin.getLogger().info(enchantment.getName());
+                //check for currency
+
+                if (player.getExp() >= plugin.getConfig().getInt("XPCost")) {
+                    //subtract currency
+                    int slot = click.getSlot();
+                    int cost = plugin.getConfig().getInt("XPCost");
+                    float xphas = player.getExp();
+
+                    pl.getInventory().getItemInMainHand().removeEnchantment(enchantmentArray[j]);
+                    click.getGui().removeElement(slot);
+                    player.setExp(xphas - cost);
+                    click.getGui().setElement(slot, new StaticGuiElement('g', stack, 1, click1 -> {return true;}, enchantmentArray[j].getName() + " Successfully Removed", "&c", "E", "e"));
+
+                //poor
+                } else {
+                    //bully them for being poor
+                    int cost = plugin.getConfig().getInt("XPCost");
+                    float xphas = player.getExp();
+                    player.sendMessage(plugin.getConfig().getString("messages.NotEnoughXP"));
+                    plugin.getLogger().info("You need: " + cost);
+                    plugin.getLogger().info("You have: " + xphas);
                 }
-                gui.draw(pl);
                 return true;
             }, enchantmentArray[j].getName())));
+
         }
         gui.addElement(enchantList);
         gui.show(pl);
---
+++ src/main/java/me/rebelmythik/disenchant/utils/EconomyProvider.java
@@ -0,0 +1,94 @@
+package me.rebelmythik.disenchant.utils;
+
+import me.rebelmythik.disenchant.DisEnchant;
+import org.bukkit.entity.Player;
+
+public class EconomyProvider {
+
+
+    public static long getBalance(DisEnchant plugin) {
+        String currencyname = plugin.getConfig().getString("EconomyProvider")
+        // Vault Economy
+
+        switch(plugin.getConfig().getString("EconomyProvider").toUpperCase()) {
+            case "VAULT" :
+
+
+                break;
+            case "EXP" :
+
+
+                break;
+            case "PECONOMY" :
+
+        }
+        if (currencyname == "Vault") {
+
+        }
+        // EXP
+        if (currencyname == "XP") {
+
+
+        // PEconomy
+        }
+        if (currencyname == "PEconomy") {
+
+        }
+        return 0;
+    }
+
+    public static void removeBalance(DisEnchant plugin) {
+        String currencyname = plugin.getConfig().getString("EconomyProvider");
+        // Vault Economy
+        if (currencyname == "Vault") {
+
+        }
+        // EXP
+        if (currencyname == "XP") {
+
+
+            // PEconomy
+        }
+        if (currencyname == "PEconomy") {
+
+        }
+    }
+
+    public static boolean hasBalance(DisEnchant plugin, Player player) {
+        String currencyName = plugin.getConfig().getString("EconomyProvider");
+        // Vault Economy
+        if (currencyName == "Vault") {
+            if (!hasEconomy(plugin)) return false;
+            return plugin.eco.hasAccount(player);
+        }
+
+        // EXP
+        if (currencyName == "XP") {
+            return true;
+
+        // PEconomy
+        }
+        if (currencyName == "PEconomy") {
+            return false;
+        }
+
+        return false;
+    }
+    public static boolean canPurchase(DisEnchant plugin, Player player, long cost) {
+        if (!hasBalance(plugin, player)) return false;
+
+        if (getBalance(plugin) < cost) return false;
+
+        return true;
+
+    }
+
+
+
+    public static boolean hasEconomy(DisEnchant plugin) {
+        if (plugin.eco == null) {
+            return false;
+        }
+        return true;
+    }
+}
